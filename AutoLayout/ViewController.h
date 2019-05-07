c43650f776c72d69bdb011357fe97bf65b149e03
Dayson Dong <Daysond@outlook.com>
2019-05-07 15:07:09 -0400

    Initial Commit

---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Dayson Dong on 2019-05-07.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayout/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AutoLayout
+//
+//  Created by Dayson Dong on 2019-05-07.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
